7df5c3cd51e8873e94366414b7f524f3a40bbb40
Nik-McKnight <nik.mcknight735@gmail.com>
2022-08-10 11:28:26 -0600

    set up schema

--- db/seed.js
+++ db/seed.js
@@ -4,19 +4,151 @@ const { createUser } = require("./models/user");
 
 const dropTables = async () => {
   console.log(`Dropping tables...`);
+  await prisma.$executeRaw`DROP TABLE IF EXISTS producttags;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS tags;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS categories;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS productphotos;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS photos;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS productsizes;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS sizes;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS cartitems;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS reviews;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS favorites;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS orders;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS products;`;
+  await prisma.$executeRaw`DROP TABLE IF EXISTS discounts;`;
   await prisma.$executeRaw`DROP TABLE IF EXISTS users;`;
   console.log("Tables dropped.");
 };
 
 const createTables = async () => {
   console.log(`Creating tables...`);
+  console.log("creating users");
   await prisma.$executeRaw`
       CREATE TABLE users (
       id SERIAL PRIMARY KEY,
+      firstname varchar(255) NOT NULL,
+      lastname varchar(255) NOT NULL,
+      username varchar(255) UNIQUE NOT NULL,
       email varchar(255) UNIQUE NOT NULL,
-      name varchar(255) 
+      shippingaddress varchar(255) ,
+      billingaddress varchar(255) ,
+      paymentinfo varchar(255) ,
+      isadmin BOOLEAN DEFAULT false,
+      createddate DATE NOT NULL DEFAULT CURRENT_DATE
+      );`;
+  console.log("creating discounts");
+
+  await prisma.$executeRaw`
+      CREATE TABLE discounts (
+      id SERIAL PRIMARY KEY,
+      name varchar(255) UNIQUE NOT NULL,
+      percent float NOT NULL,
+      couponCode varchar(255) UNIQUE NOT NULL
       );
 `;
+  console.log("creating products");
+
+  await prisma.$executeRaw`
+        CREATE TABLE products (
+        id SERIAL PRIMARY KEY,
+        name varchar(255),
+        description varchar(255),
+        price float NOT NULL,
+        discountid INTEGER REFERENCES discounts(id),
+        createddate DATE NOT NULL DEFAULT CURRENT_DATE,
+        numbersold INTEGER NOT NULL DEFAULT 0,
+        rating FLOAT DEFAULT 5.0
+        );
+  `;
+  console.log("creating orders");
+
+  await prisma.$executeRaw`
+        CREATE TABLE orders (
+        id SERIAL PRIMARY KEY,
+        discountid INTEGER REFERENCES discounts(id),
+        userid INTEGER REFERENCES users(id),
+        shippingaddress varchar(255) ,
+        billingaddress varchar(255) ,
+        fulfilled BOOLEAN DEFAULT false,
+        paymentinfo varchar(255) ,
+        subtotal float NOT NULL DEFAULT 0.0,
+        tax float NOT NULL DEFAULT 0.0,
+        total float NOT NULL DEFAULT 0.0,
+        orderdate DATE NOT NULL DEFAULT CURRENT_DATE
+        );
+  `;
+  console.log("creating favorites");
+
+  await prisma.$executeRaw`
+      CREATE TABLE favorites (
+      id SERIAL PRIMARY KEY,
+      userid INTEGER REFERENCES users(id),
+      productid INTEGER REFERENCES products(id),
+      favoriteddate DATE NOT NULL DEFAULT CURRENT_DATE
+      );
+  `;
+  await prisma.$executeRaw`
+    CREATE TABLE reviews (
+    id SERIAL PRIMARY KEY,
+    userid INTEGER REFERENCES users(id),
+    productid INTEGER REFERENCES products(id),
+    rating INTEGER NOT NULL,
+    content varchar(255) NOT NULL
+    );`;
+  await prisma.$executeRaw`
+      CREATE TABLE cartitems (
+      id SERIAL PRIMARY KEY,
+      orderid INTEGER REFERENCES orders(id),
+      productid INTEGER REFERENCES products(id),
+      quantity INTEGER NOT NULL DEFAULT 1,
+      subtotal FLOAT DEFAULT 0.0
+      );
+  `;
+  await prisma.$executeRaw`
+  CREATE TABLE sizes (
+    id SERIAL PRIMARY KEY,
+    size FLOAT UNIQUE NOT NULL
+  );`;
+  await prisma.$executeRaw`
+      CREATE TABLE productsizes (
+      id SERIAL PRIMARY KEY,
+      productid INTEGER REFERENCES products(id),
+      sizeid INTEGER REFERENCES sizes(id),
+      inventory INTEGER NOT NULL DEFAULT 0
+      );
+  `;
+  await prisma.$executeRaw`
+  CREATE TABLE photos(
+    id SERIAL PRIMARY KEY,
+    name varchar(255) NOT NULL,
+    url varchar(255) NOT NULL
+  );`;
+  await prisma.$executeRaw`
+    CREATE TABLE productphotos (
+    id SERIAL PRIMARY KEY,
+    productid INTEGER REFERENCES products(id),
+    photoid INTEGER REFERENCES photos(id)
+    );
+  `;
+  await prisma.$executeRaw`
+  CREATE TABLE categories(
+    id SERIAL PRIMARY KEY,
+    name varchar(255) UNIQUE NOT NULL
+  );`;
+  await prisma.$executeRaw`
+  CREATE TABLE tags(
+    id SERIAL PRIMARY KEY,
+    name varchar(255) UNIQUE NOT NULL,
+    categoryid INTEGER REFERENCES categories(id)
+  );`;
+  await prisma.$executeRaw`
+    CREATE TABLE producttags (
+    id SERIAL PRIMARY KEY,
+    productid INTEGER REFERENCES products(id),
+    tagid INTEGER REFERENCES tags(id)
+    );
+  `;
   console.log("Tables built");
 };
 
@@ -24,7 +156,7 @@ const seedDb = async () => {
   console.log("Creating initial users...");
   const user = { email: "123@xyz.com", name: "test" };
   console.log(user);
-  const createdUser = await createUser(user);
+  const createdUser = await prisma.users.create({ data: user });
   console.log(createdUser);
 };
 
