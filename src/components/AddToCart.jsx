83440065eefa10b83b7eba79a25bf6063a29e9cb
gavinstark81 <gavinstark81@gmail.com>
2022-08-19 16:28:44 -0500

    still working on increase, decrease, and delete button

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,15 +1,23 @@
 import React, { useState, useEffect } from "react";
 import { getProduct } from "../api/products";
-import { useLocation, useParams } from "react-router-dom";
+import { Navigate, useLocation, useParams } from "react-router-dom";
 import { fetchCart } from "../api/orders";
-import { addToCart } from "../api/cartItems";
+import {
+  addToCart,
+  decreaseQty,
+  increaseQty,
+  removeFromCart,
+} from "../api/cartItems";
+import { useNavigate } from "react-router-dom";
 
 export default function AddToCart() {
   const [product, setProduct] = useState([]);
   const [orderId, setOrderId] = useState([]);
   const [cartItem, setCartItem] = useState();
+  const [quantity, setQuantity] = useState();
   const localShoeId = JSON.parse(localStorage.getItem("shoeid"));
   const localSizeId = JSON.parse(localStorage.getItem("sizeid"));
+  let navigate = useNavigate();
 
   useEffect(() => {
     const getOneProduct = async () => {
@@ -36,7 +44,7 @@ export default function AddToCart() {
     };
     getCartItem();
   }, []);
-
+  console.log("cart item", cartItem);
   return (
     <div>
       <h2>{product.name}</h2>
@@ -51,9 +59,32 @@ export default function AddToCart() {
         width="250"
         height="250"
       />
+
       <form>
-        <output name="calculate">{cartItem.quantity}</output>
-        <button></button>
+        {/* <output name="calculate" for={cartItem}></output> */}
+        <h4>{quantity}</h4>
+
+        <button
+          onClick={() => {
+            decreaseQty(12);
+          }}
+        >
+          -
+        </button>
+        <button
+          onClick={async () => {
+            await increaseQty(2); // increase decrease and delete all work in thunder but still need to get it working on front end
+          }}
+        >
+          +
+        </button>
+        <button
+          onClick={() => {
+            removeFromCart(12);
+          }}
+        >
+          Delete
+        </button>
       </form>
     </div>
   );
