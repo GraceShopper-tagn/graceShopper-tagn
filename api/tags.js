ff4e020f3624636f02611c015ccc5eefcf8477fa
tomrrees <thomasrrees@gmail.com>
2022-08-22 15:31:25 -0600

    completed drop down list filter in the tags component

--- api/tags.js
+++ api/tags.js
@@ -5,11 +5,10 @@ const prisma = require("../db/prisma");
 tagsRouter.get("/", async (req, res, next) => {
   try {
     const tags = await prisma.tags.findMany({
-      include: {
-        id: true,
-        name: true,
-        categoryid: true,
-      },
+      //   include: {
+      //     categories: true,
+      //     producttags: true,
+      //   },
     });
     res.send(tags);
   } catch (error) {
---
+++ src/api/tags.js
@@ -0,0 +1,11 @@
+export const getAllTags = async () => {
+  const response = await fetch(`/api/tags`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+  const result = await response.json();
+  //   console.log("result", result);
+  return result;
+};
