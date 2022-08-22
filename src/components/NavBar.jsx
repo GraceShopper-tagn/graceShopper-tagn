2f4b07209dd7a9aaa467d25856d0ab84dfa9b5cc
acr23xx <alexramirez.312@gmail.com>
2022-08-22 16:06:06 -0700

    css styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,50 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
+import { FullNav, NavP } from "./styles/navbar";
+import "bootstrap/dist/css/bootstrap.css";
+import { DropdownButton } from "react-bootstrap";
+import { ListGroupItem } from "react-bootstrap";
+import { AiOutlineSearch } from "react-icons/ai";
+import styles from "./styles/navbar.css";
 import useAuth from "../hooks/useAuth";
 
 export default function NavBar() {
   const { user } = useAuth();
   return (
-    <div>
-      <header className="header-format">
-        <h1>Shoes</h1> <h3>(Shop at Your Own Risk)</h3>
-      </header>
-      <nav className="navbar">
+    <nav>
+      <FullNav>
+        <DropdownButton style={{ background: "#DA9C6C" }} title="SUPPORT">
+          <ListGroupItem key="1">Contact Info</ListGroupItem>
+          <ListGroupItem key="2">FAQ</ListGroupItem>
+          <ListGroupItem key="2">Give Us Feedback</ListGroupItem>
+        </DropdownButton>
+        <NavP>
+          TAGN specializes in assisting their customers in every way possible!{" "}
+          <strong>Enjoy our products</strong>
+        </NavP>
+
+        <div className={styles.searchNav}>
+          <div>
+            <img
+              onClick={() => Navigate("/")}
+              src="logo.png"
+              alt="group logo"
+              style={{ cursor: "pointer" }}
+              height="125px"
+            />
+          </div>
+          <div>
+            <AiOutlineSearch
+              style={{ position: "absolute", width: "20px", height: "40px" }}
+            />
+            <input type="search" placeholder="Find your soles" />
+            <button> SEARCH </button>
+          </div>
+        </div>
+
+        <header className="header-format">
+          <h1>Shoes</h1>
+        </header>
         <Link className="links" to="/products">
           Store
         </Link>
@@ -44,7 +79,7 @@ export default function NavBar() {
             Administrator
           </Link>
         )}
-      </nav>
-    </div>
+      </FullNav>
+    </nav>
   );
 }
---
+++ src/components/styles/navbar.js
@@ -0,0 +1,19 @@
+import styled from "styled-components";
+
+export const FullNav = styled.div`
+  height: 155px;
+  background-color: #c14836;
+  display: flex;
+  flex-direction: column;
+
+  h1 {
+    color: #decb7f;
+    margin-left: 10%;
+  }
+`;
+export const NavP = styled.p`
+  color: black;
+  font-size: 18px;
+  margin-left: 10%;
+  margin-top: 5px;
+`;
